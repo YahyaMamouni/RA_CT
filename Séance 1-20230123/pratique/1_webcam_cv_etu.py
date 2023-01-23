2668d9639a5be781e9ad40874aa418d6a45074a2
Yahya MAMOUNI <yamamouni@local.isima.fr>
2023-01-23 17:11:27 +0100

    Exercice 1

--- Séance 1-20230123/pratique/1_webcam_cv_etu.py
+++ Séance 1-20230123/pratique/1_webcam_cv_etu.py
@@ -22,10 +22,45 @@ while(True):
     # Traitements à compléter ici : détection des points d'intérêt
     # Harris corner, SIFT, FAST, SURF...
 
+        
+    """
+    # SIFT
+    # Create a SIFT object
+    sift = cv2.xfeatures2d.SIFT_create()
+    
+    # Detect and describe the features
+    keypoints, descriptors = sift.detectAndCompute(gray, None)
+    
+    # Draw the keypoints on the image
+    frame = cv2.drawKeypoints(gray, keypoints, frame)
+    """
+    
+    
+    # Harris
+
+    corners = cv2.goodFeaturesToTrack(gray, 250, 0.01, 10)
+    corners = np.int0(corners)
+ 
+    for i in corners:
+        x, y = i.ravel()
+        cv2.circle(frame, (x, y), 3, (0, 255, 0), -1)
+    
+    """    
+    #FAST
+    # Create a FAST object
+    fast = cv2.FastFeatureDetector_create()
+    
+    # Detect corners
+    keypoints = fast.detect(gray, None)
+    
+    # Draw corners
+    frame = cv2.drawKeypoints(gray, keypoints, frame)
+   """
    
     # Affichage de l'image
     cv2.imshow('gray frame',gray)
     cv2.imshow('color frame',frame)
+    #cv2.imshow('harris frame', harris)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
